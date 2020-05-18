30d439cd3e1c24407828305950dfc56bc2c043d2
bclayton314 <62265654+bclayton314@users.noreply.github.com>
2020-05-18 08:06:56 -0500

    Add files via upload

---
+++ Algos & Exercises/Algos_Structs1/Algos_Structs1/Algo_Structs1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algos_Structs1
+{
+    class Algo_Structs1
+    {
+        //Remove Duplicates from Sorted Array
+        /*Given a sorted array nums, remove the duplicates 
+         * in-place such that each element appear only once 
+         * and return the new length.
+          Do not allocate extra space for another array, 
+          you must do this by modifying the input array 
+          in-place with O(1) extra memory.
+        */
+        static void Main(string[] args)
+        {
+            //int[] array = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+            //int endOfList = DeleteDuplicates(array);
+
+            //for (int i = 0; i < endOfList; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+
+           
+        }
+
+        public static int DeleteDuplicates(int[] array)
+        {
+            if (array.Length == 0)
+                return 0;
+
+            int endOfList = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[endOfList] != array[i])
+                {
+                    endOfList++;
+                    array[endOfList] = array[i];
+                }
+                
+            }
+            return endOfList + 1;
+        }
+
+        //Rotate Array
+        /*Given an array, rotate the array to the right 
+        by k steps, where k is non-negative.
+
+        Follow up:
+
+        Try to come up as many solutions as you can, 
+        there are at least 3 different ways to solve 
+        this problem.
+        Could you do it in-place with O(1) extra space?
+        */
+
+        //public static int RotateArray(int[] array1, int k)
+        //{
+        //    k = array1.Length;
+        //    reverse(array1, 0, array1.Length - 1);
+        //    reverse(array1, 0, k - 1);
+        //    reverse(array1, k, array1.Length - 1);
+        //}
+        //public void reverse(int[] array1, int start, int end)
+        //{
+        //    while (start < end)
+        //    {
+        //        int temp = array1[start];
+        //        array1[start] = array1[end];
+        //        array1[end] = temp;
+        //        start++;
+        //        end--;
+        //    }
+        //}
+
+        //public static int MultThreeFive()
+        //{
+        //    int limit = 1000;
+        //    int sum = 0;
+
+        //    for (int i = 0; i < limit; i++)
+        //    {
+        //        if (i % 3 == 0 || i % 5 == 0)
+        //            sum += i;
+        //    }
+
+        //    return sum;
+
+
+        //}
+    }
+}
---
+++ Algos & Exercises/Even_Fibonacci_Num/Even_Fibonacci_Num/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Fibonacci_Num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = 4000000;
+            Console.Write(evenFibSum(limit));
+            Fibonacci();
+        }
+
+        public static void Fibonacci()
+        {
+            int result = 0;
+
+            int fibNum1 = 1;
+            int fibNum2 = 1;
+
+            while (fibNum1 <= 4000000)
+            {
+                if (fibNum1 % 2 == 0)
+                    result += fibNum1;
+
+                int fibNum2Aux = fibNum1;
+                fibNum1 = fibNum2 + fibNum1;
+                fibNum2 = fibNum2Aux;
+            }
+            Console.WriteLine("Sum of even fibonacci numbers below 4 million is: " + result);
+        }
+
+        static int evenFibSum(int limit)
+        {
+            if (limit < 2)
+                return 0;
+
+            // Initialize first two even 
+            // prime numbers and their sum 
+            long ef1 = 0, ef2 = 2;
+            long sum = ef1 + ef2;
+
+            // calculating sum of even  
+            // Fibonacci value 
+            while (ef2 <= limit)
+            {
+
+                // get next even value of  
+                // Fibonacci sequence 
+                long ef3 = 4 * ef2 + ef1;
+
+                // If we go beyond limit, 
+                // we break loop 
+                if (ef3 > limit)
+                    break;
+
+                // Move to next even number 
+                // and update sum 
+                ef1 = ef2;
+                ef2 = ef3;
+                sum += ef2;
+            }
+
+            return (int)sum;
+        }
+    }
+}
