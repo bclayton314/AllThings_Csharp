321abaf8fa0c6c7de8036488ba3ddb214731a2a8
bclayton314 <62265654+bclayton314@users.noreply.github.com>
2020-05-18 08:09:08 -0500

    Add files via upload

---
+++ CSC205/HmwrkWeek1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC205_HmwrkWeek1
+{
+    class HmwrkWeek1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MSSA! My name is Clayton Baker.");
+        }
+    }
+}
