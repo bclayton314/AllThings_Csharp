3d8926f4f7e0fb50571ea101e72230d8ec9f2716
bclayton314 <62265654+bclayton314@users.noreply.github.com>
2020-06-06 11:21:26 -0500

    Add files via upload

---
+++ CSC205/CSC205_Quiz4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC205_Quiz4
+{
+    class CSC205_Quiz4
+    {
+
+
+        static void Main(string[] args)
+        {
+            int x = userSum("Please enter 1st number: "); //Calling method with display message
+            int y = userSum("Please enter 2nd number: "); //Calling method with second message
+
+            int intSum = x + y; //Adding the two user inputs
+            Console.WriteLine("Your sum is {0}.", intSum); //Displaying sum
+
+        }
+        static public int userSum(string str1)
+        {  
+
+
+            Console.WriteLine(str1);    //Printing the string parameter asking for user input
+            int num1 = int.Parse(Console.ReadLine()); //Converting to integer type
+            return num1; //Returning an integer value
+            
+        }
+    }
+}
+            
---
+++ CSC205/Hmwrk_Week4.1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hmwrk_Week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //int j = 0;
+            //int[] storeVal = new int[5];
+
+            //while (i <= 4)
+            //{
+            //    Console.WriteLine("please enter the sales values (only multiples of 100 please) for store {0}: ", i + 1);
+            //    storeVal[i] = int.Parse(Console.ReadLine());
+            //    i++;
+            //}
+
+            //Console.WriteLine("\nSALES BAR CHART\n(Each * = $100)");
+
+            //while (j <= 4)
+            //{
+            //    //used int becasue we're dealing with whole numbers,
+            //    //would change to float if necessary
+            //    int storeVal2 = storeVal[j] / 100;
+            //    string storeDisplay = DisplayAsterisks(storeVal2);
+            //    Console.Write($"Store {j + 1}: {storeDisplay}\n");
+            //    j++;
+            //}
+
+
+            //Run a test, find out why the symbols keep being flushed to the left
+            //https://docs.microsoft.com/en-us/dotnet/csharp/methods
+            Console.WriteLine("Asterisks: " + DisplayAsterisks(5));
+
+        }
+
+        public static string DisplayAsterisks(int num)
+        {
+            char aster1 = char.Parse("*");
+            int i = 1;
+            while (i <= num)
+            {
+                Console.Write(aster1);
+                i++;
+            }
+            return null;
+        }
+    }
+}
