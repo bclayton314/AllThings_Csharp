9cbb7537e8be2859a7581bc30ed787dd0f06b7ae
bclayton314 <62265654+bclayton314@users.noreply.github.com>
2020-09-01 15:36:29 -0500

    Add files via upload

---
+++ JSONParser/jsonPersonComplex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONParser
+{
+    class jsonPersonComplex : jsonPersonSimple
+    {
+        /*
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public double age { get; set; }
+        public bool isAlive { get; set; }
+        */
+        public addr address { get; set; }
+
+        public class addr
+        {
+            public string streetAddress { get; set; }
+            public string city { get; set; }
+            public string postCode { get; set; }
+        }
+    }
+}
---
+++ JSONParser/jsonPersonSimple.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+{
+  "firstname" : "Roger",
+  "lastname" : "Moore",
+  "age" : 89,
+  "isAlive" : false,
+  "address" :
+  {
+    "streetAddress" : "1 Main Street",
+    "city" : "London",
+    "postCode" : "N1 3XX"
+  },
+    "phoneNumbers" :
+    [
+        {"type" : "home", "number" : "+61 03 1234 5678" },
+        {"type" : "mobile", "number" : "+61 0405 111 222"}
+    ]  
+}
+*/
+namespace JSONParser
+{
+    class jsonPersonSimple
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public double age { get; set; }
+        public bool isAlive { get; set; }
+    }
+}
